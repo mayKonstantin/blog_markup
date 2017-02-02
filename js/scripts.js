d31b9889efcd900a6b331e79544a33eeb6294d9f
Константин Майданцев <kmaydantsev@ronasit.com>
2017-02-02 21:46:24 +0600

    Ready for angular2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,42 @@
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-})
+$(document).ready(function() {
+  
+  // Bootstrap toolip
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  // Scroll to anchor
+  $('a[href*=#]').on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 1000, 'linear');
+	});
+
+  // Add behaviour to elements while scrolling
+  $(window).bind('scroll', function (e) {
+
+    if ($(window).scrollTop() > 420) {
+        $('.button-block').addClass('fixed');
+        $('.scroller').addClass('hidden');
+    } else {
+        $('.button-block').removeClass('fixed');
+        $('.scroller').removeClass('hidden');
+    }
+
+    if ($(window).scrollTop() >= 695) {
+      $('.button-block').removeClass('fixed');
+      $('.button-block').addClass('absolute_menu');
+    } else {
+      $('.button-block').removeClass('absolute_menu');
+    }
+
+    if ($(window).scrollTop() >= 670) {
+      $('.fa-linkedin-square').addClass('absolute');
+      $('.contact_information').addClass('absolute');
+    } else {
+      $('.fa-linkedin-square').removeClass('absolute');
+      $('.contact_information').removeClass('absolute');
+    }
+  });
+  
+
+});
